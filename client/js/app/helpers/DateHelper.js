d2a265e6db9f166e5d5d1b0656d8850b777affb8
Isengar <ricci.gr@gmail.com>
2017-12-10 18:45:28 -0200

    criando lista de negociacoes

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,21 +1,21 @@
 class DateHelper{
 
   constructor(){
-    throw new Error('Esta class nao pode ser instanciada');
+      throw new Error('Esta class nao pode ser instanciada');
   }
 
   static textoParaData(texto){
 
-    let expressaoData = new RegExp(/^\d{4}-\d{2}-\d{2}$/);
+      let expressaoData = new RegExp(/^\d{4}-\d{2}-\d{2}$/);
 
-    if(!expressaoData.test(texto))
-        throw new Error('Formato invalido. Utilizar aaaa-mm-dd.');
+      if(!expressaoData.test(texto))
+          throw new Error('Formato invalido. Utilizar aaaa-mm-dd.');
 
-    return new Date(
-      ...texto.split('-').map((item, index) => item - index % 2));
+      return new Date(
+        ...texto.split('-').map((item, index) => item - index % 2));
   }
 
   static dataParaTexto(data){
-    return  "${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}";
+      return  "${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}";
   }
 }
