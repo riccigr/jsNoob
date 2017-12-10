15ce8492c261fc7a14cc144b48cf59e8af988941
Isengar <ricci.gr@gmail.com>
2017-12-10 18:20:53 -0200

    melhorando controller com templateStringe regex

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,13 +5,17 @@ class DateHelper{
   }
 
   static textoParaData(texto){
+
+    let expressaoData = new RegExp(/^\d{4}-\d{2}-\d{2}$/);
+
+    if(!expressaoData.test(texto))
+        throw new Error('Formato invalido. Utilizar aaaa-mm-dd.');
+
     return new Date(
       ...texto.split('-').map((item, index) => item - index % 2));
   }
 
   static dataParaTexto(data){
-    return data.getDate()
-            + '/' + (data.getMonth() + 1)
-            + '/' + data.getFullYear();
+    return  "${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}";
   }
 }
