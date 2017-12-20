d19fa94ad3b07004205f3d5763d843e9c16c456a
Isengar <ricci.gr@gmail.com>
2017-12-19 22:22:19 -0200

    refatorando NegociacaoService com Promise

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -1,78 +1,50 @@
 class NegociacaoService {
 
+    constructor(){
+        this._http = new HttpService();
+    }
+
     obterNegociacoesSemana(){
-
         return new Promise((resolve, reject) => {
-
-            const DONE = 4;
-            let xhr = new XMLHttpRequest();
-
-            xhr.open('GET', 'negociacoes/semana');
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == DONE){
-                    if(xhr.status == 200){
-
-                        resolve(JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-
-                    }else{
-                        console.log(xhr.responseText);
-                        reject('Não foi possível importar as negociações da semana.');
-                    }
-                }
-            };
-            xhr.send();
+            this._http
+                .get('negociacoes/semana')
+                .then( objeto  => {
+                    resolve(objeto.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject('Não foi possível importar as negociações da semana.');
+                });
         });
     }
 
     obterNegociacoesAnterior(){
 
         return new Promise((resolve, reject) => {
-
-            const DONE = 4;
-            let xhr = new XMLHttpRequest();
-
-            xhr.open('GET', 'negociacoes/anterior');
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == DONE){
-                    if(xhr.status == 200){
-
-                        resolve(JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-
-                    }else{
-                        console.log(xhr.responseText);
-                        reject('Não foi possível importar as negociações da semana anterior.');
-                    }
-                }
-            };
-            xhr.send();
+            this._http
+                .get('negociacoes/anterior')
+                .then( objeto  => {
+                    resolve(objeto.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject('Não foi possível importar as negociações da semana anterior.');
+                });
         });
     }
 
     obterNegociacoesRetrasada(){
 
         return new Promise((resolve, reject) => {
-
-            const DONE = 4;
-            let xhr = new XMLHttpRequest();
-
-            xhr.open('GET', 'negociacoes/retrasada');
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == DONE){
-                    if(xhr.status == 200){
-
-                        resolve(JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-
-                    }else{
-                        console.log(xhr.responseText);
-                        reject('Não foi possível importar as negociações da semana retrasada.');
-                    }
-                }
-            };
-
-            xhr.send();
+            this._http
+                .get('negociacoes/retrasada')
+                .then( objeto  => {
+                    resolve(objeto.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject('Não foi possível importar as negociações da semana retrasada.');
+                });
         });
 
     }
